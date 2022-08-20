478f6cfb085c0e65177ad8a8b45dd4aa2b1ea59f
WinstonSmith314 <tobias.reidarsen@gmail.com>
2022-08-21 00:19:34 +0200

    wewer
    wqerwqer

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+app = document.getElementById("app");
+
+// VIEW
+window.onload
+view();
+function view(){
+    let html = /*HTML*/``;
+
+    html += /*HTML*/`
+        <button>Click</button>
+    `;
+
+    app = html;
+}
